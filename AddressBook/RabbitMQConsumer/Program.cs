1298830f55d03c6c46dc61266de59919db86c73a
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-03-16 22:29:32 +0530

    [komal]Adding Section4_UC1 code

---
+++ AddressBook/RabbitMQConsumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Microsoft.Extensions.Configuration;
+using System;
+using ModelLayer.DTO;
+
+namespace RabbitMQ.Consumer
+{
+    /// <summary>
+    /// class to consume the RabbitMq publish on channels
+    /// </summary>
+    public class UserConsumer
+    {
+        private readonly IConfiguration _configuration;
+
+        //constructor
+        public UserConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// method to consume the Message from RabbitMQ
+        /// </summary>
+        public void ConsumeMessage()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMQ:Host"],
+                UserName = _configuration["RabbitMQ:UserName"],
+                Password = _configuration["RabbitMQ:Password"]
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: _configuration["RabbitMQ:Queue"], durable: true, exclusive: false, autoDelete: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var userEvent = JsonSerializer.Deserialize<UserEventDTO>(message);
+
+                Console.WriteLine($"[User Event Received] Name: {userEvent.FirstName} {userEvent.LastName}, Email: {userEvent.Email}");
+
+                // Simulate processing time
+                System.Threading.Thread.Sleep(1000);
+            };
+
+
+            channel.BasicConsume(queue: _configuration["RabbitMQ:Queue"], autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Listening for messages...");
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+            Console.WriteLine("RabbitMQ Host: " + configuration["RabbitMQ:Host"]);
+
+            UserConsumer consumer = new UserConsumer(configuration);
+            consumer.ConsumeMessage();
+
+        }
+    }
+}
+
