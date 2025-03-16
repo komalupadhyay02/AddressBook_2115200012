10e1d9bb045a6eb8f5212647c59bd19c8b57322e
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-03-16 22:16:08 +0530

    [komal]Adding Section2_UC2 code

---
+++ AddressBook/RepositoryLayer/Context/AddressBookContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.Model;
+namespace RepositoryLayer.Context
+{
+	public class AddressBookContext:DbContext
+	{
+		public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options) { }
+		public DbSet<AddressBookEntry> AddressBook { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
+
---
+++ AddressBook/RepositoryLayer/Context/AddressBookContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace RepositoryLayer.Context
+{
+    public class AddressBookContextFactory : IDesignTimeDbContextFactory<AddressBookContext>
+    {
+        public AddressBookContext CreateDbContext(string[] args)
+        {
+            // Read configuration from appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AddressBookContext>();
+            var connectionString = config.GetConnectionString("Sqlserver");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AddressBookContext(optionsBuilder.Options);
+        }
+    }
+}
