411ca75903c8e6fbe368a27e5d5fa84192c27e63
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-03-16 22:19:44 +0530

    [komal]Adding Sectin3_UC1 code

---
+++ AddressBook/ModelLayer/Model/AddressBookEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ModelLayer.Model
+{
+	public class AddressBookEntry
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[EmailAddress]
+		[Required]
+		public string Email { get; set; }
+		[Phone]
+		[Required]
+		public string Phone_No { get; set; }
+		public int UserId { get; set; }
+	}
+}
+
---
+++ AddressBook/ModelLayer/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ModelLayer.Model
+{
+	public enum Role
+	{
+		Admin,User
+	}
+	public class User
+	{
+		[Key]
+		public int UserId { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+
+		[Required,EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		public string PasswordHash { get; set; }
+		[Required]
+		public Role UserRole { get; set; } = Role.User;
+
+		public string? ResetToken { get; set; }
+		public DateTime? ResetTokenExpiry { get; set; }
+	}
+}
+
